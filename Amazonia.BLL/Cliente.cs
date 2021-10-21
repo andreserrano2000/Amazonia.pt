de0b60aef4721f308f39ebdd1ff5f661cfd84396
andre.m.c.serrano <andre.m.c.serrano>
2021-10-21 18:51:44 +0100

    Second Commit New Changes

--- Amazonia.BLL/Cliente.cs
+++ Amazonia.BLL/Cliente.cs
@@ -1,6 +1,6 @@
 class Cliente
 {
-    public string Nome { get; set; }    
+    public string Nome { get; set; }
     public Morada Morada { get; set; }
     public string UserName { get; set; }
     public string Password { get; set; }
---
+++ Amazonia.DAL/LivroDigital.cs
@@ -0,0 +1,15 @@
+
+namespace Amazonia.DAL
+{
+    public class LivroDigital : Livro
+    {
+        public int TamanhoEmMB { get; set; }
+        public string FormatoFicheiro { get; set; }  //PDF, DOC, EPUB ....
+        public string InformacoesLicenca { get; set; }
+
+        public override decimal ObterPreco()
+        {
+            return base.ObterPreco() * 0.9M;
+        }
+    }
+}
